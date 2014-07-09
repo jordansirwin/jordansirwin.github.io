c581f3379cf181c4a5dcd35d22afbc6de7486952
jirwin <jordan.s.irwin@gmail.com>
2014-07-09 13:30:09 -0700

    cleaning

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "Building..."
-jekyll build -s .. -d ../_site
-echo "Copying..."
-cp -r ../_site/ ../../master/
-git --git-dir=../../master/.git add --all
-git --git-dir=../../master/.git commit -am "Deploying"
-git --git-dir=../../master/.git push origin master
