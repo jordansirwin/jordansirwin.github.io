d8cf2dcc24cc617d0fedea00cf5accc03f356f87
jirwin <jordan.s.irwin@gmail.com>
2014-07-09 13:28:26 -0700

    Deploying

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ echo "Copying..."
 cp -r ../_site/ ../../master/
 git --git-dir=../../master/.git add --all
 git --git-dir=../../master/.git commit -am "Deploying"
+git --git-dir=../../master/.git push origin master
