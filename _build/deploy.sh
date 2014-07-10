5c5d3205c77772584a97a5404e2039a9d46f39b1
jirwin <jordan.s.irwin@gmail.com>
2014-07-09 18:21:13 -0700

    testing

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -1,8 +1,14 @@
 echo "Building..."
 jekyll build -s .. -d ../_site
+echo "Deleting..."
+find ../../master -regex ".*/master/[^.].*" -exec rm -rf {} \;
 echo "Copying..."
 cp -r ../_site/ ../../master/
-git --git-dir=../../master/.git add --all
-git --git-dir=../../master/.git status
-echo git --git-dir=../../master/.git commit -am "Deploying"
-echo git --git-dir=../../master/.git push origin master
+echo "Adding..."
+git --git-dir=../../master/.git --work-tree=../../master/ add --all
+echo "Committing..."
+echo git --git-dir=../../master/.git --work-tree=../../master/ status
+git --git-dir=../../master/.git --work-tree=../../master/ commit -am "Deploying"
+echo "Pushing..."
+git --git-dir=../../master/.git --work-tree=../../master/ push origin master
+echo "Done!"
