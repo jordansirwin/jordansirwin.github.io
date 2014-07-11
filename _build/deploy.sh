5a26a1dd138e37661530326c07b5ed88267020b5
jirwin <jordan.s.irwin@gmail.com>
2014-07-10 23:14:04 -0700

    custom theme with less

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -1,13 +1,15 @@
-echo "Building..."
+echo "Compiling less"
+lessc ../content/self/self.less > ../content/self/self.css
+echo "Building _site"
 jekyll build -s .. -d ../_site
-echo "Deleting..."
+echo "Deleting master"
 find ../../master -regex ".*/master/[^.].*" -exec rm -rf {} \;
-echo "Copying..."
+echo "Copying to master"
 cp -r ../_site/ ../../master/
-echo "Adding..."
+echo "Adding to git:master"
 git --git-dir=../../master/.git --work-tree=../../master/ add --all
-echo "Committing..."
+echo "Committing to git:master"
 git --git-dir=../../master/.git --work-tree=../../master/ commit -am "Deploying"
-echo "Pushing..."
+echo "Pushing to github pages:master"
 git --git-dir=../../master/.git --work-tree=../../master/ push origin master
 echo "Done!"
